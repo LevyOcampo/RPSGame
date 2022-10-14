65d69f29bfbce83585070df70720190278f0b88e
LevyOcampo <levymocampo@gmail.com>
2022-10-14 09:32:43 -0700

    Created a function that will randomly choose the computers move in the game

--- code.js
+++ code.js
@@ -1,1 +1,11 @@
-console.log("HI")
+function getComputerChoice(){
+    x = Math.floor(Math.random()*91)
+    if(x<=30){
+        return "Rock"
+    }
+    else if (x<=60 && x>=31){
+        return "Paper"
+    } else{
+        return "Scissors"
+    }
+}
