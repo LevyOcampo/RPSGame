49a11601c41121c479f5418905a0cd8281f334cc
LevyOcampo <levymocampo@gmail.com>
2022-10-14 09:50:23 -0700

    Created a playRound function that checks to see all possible combinations of plays and returns a string with the outcome

--- code.js
+++ code.js
@@ -9,3 +9,40 @@ function getComputerChoice(){
         return "Scissors"
     }
 }
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection == "Rock"){
+
+        if(computerSelection == "Rock"){
+            return "It's a tie"
+        } else if(computerSelection == "Paper"){
+            return "Paper beats Rock, Sorry you lose"
+        }else{
+            return "Rock beats Scissors, Congrats you win"
+        }
+
+    } else if(playerSelection == "Paper"){
+
+        if(computerSelection == "Rock"){
+            return "Paper beats Rock, Congrats you win"
+        } else if(computerSelection == "Paper"){
+            return "It's a tie"
+        }else{
+            return "Scissors beats Paper, Sorry you lose"
+        }
+
+    }else if(playerSelection == "Scissors"){
+
+        if(computerSelection == "Rock"){
+            return "Rock beats Scissors, Sorry you lose"
+        } else if(computerSelection == "Paper"){
+            return "Scissors beats Paper, Congrats you win"
+    
+        }else{
+            return "It's a tie"
+        }
+
+    }else{
+        return "Please enter a valid play"
+    }
+}
