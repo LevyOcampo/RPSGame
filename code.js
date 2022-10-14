7c914cdec97009a5b30afb29c6ee8af09742f2c6
LevyOcampo <levymocampo@gmail.com>
2022-10-14 10:42:52 -0700

    Created a game function that calls on 5 rounds using a loop and based on each rounds outcome the function determines a winner after the 5 rounds

--- code.js
+++ code.js
@@ -1,3 +1,6 @@
+var playerScore = 0;
+var computerScore = 0;
+
 function getComputerChoice(){
     x = Math.floor(Math.random()*91)
     if(x<=30){
@@ -29,26 +32,32 @@ function playRound(playerSelection, computerSelection){
         if(computerSelection == "Rock"){
             return "It's a tie"
         } else if(computerSelection == "Paper"){
+            computerScore++;
             return "Paper beats Rock, Sorry you lose"
         }else{
+            playerScore++
             return "Rock beats Scissors, Congrats you win"
         }
 
     } else if(playerSelection == "paper"){
 
         if(computerSelection == "Rock"){
+            playerScore++
             return "Paper beats Rock, Congrats you win"
         } else if(computerSelection == "Paper"){
             return "It's a tie"
         }else{
+            computerScore++
             return "Scissors beats Paper, Sorry you lose"
         }
 
     }else if(playerSelection == "scissors"){
 
         if(computerSelection == "Rock"){
+            computerScore++
             return "Rock beats Scissors, Sorry you lose"
         } else if(computerSelection == "Paper"){
+            playerScore++
             return "Scissors beats Paper, Congrats you win"
     
         }else{
@@ -60,7 +69,23 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-const playerSelection = getPlayerChoice();
-const computerSelection = getComputerChoice();
 
-console.log(playRound(playerSelection, computerSelection));
+function game(){
+    playerScore = 0;
+    computerScore = 0;
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+            
+    if(playerScore>computerScore){
+        return "congrats you win! Your score: " +playerScore+ " Computer score: " +computerScore
+    }else if(computerScore>playerScore){
+        return "sorry you lose. Your score: " +playerScore+ " Computer score: " +computerScore
+    }else{
+        return "it's a tie! Your score: " +playerScore+ " Computer score: " +computerScore
+    }
+}
+
+console.log(game());
